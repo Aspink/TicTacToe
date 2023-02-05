f8d8e1e722538e14370e73f26cc2d92a56766fc5
Aspink <aspink1@wp.pl>
2023-02-05 21:10:14 +0100

    TicTacToe project created, board tested.

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("  1  2  3 ");
+        System.out.println("1 X  O  X");
+        System.out.println("2    O  X");
+        System.out.println("3 X  O   ");
+    }
+}
