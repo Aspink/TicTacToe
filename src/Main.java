e8fe358524418b60c8438bd69f703750fc10f42e
Aspink <aspink1@wp.pl>
2023-02-07 21:47:32 +0100

    TicTacToe some stuff removed from Main class.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("  1  2  3 ");
-        System.out.println("1 X  O  X");
-        System.out.println("2    O  X");
-        System.out.println("3 X  O   ");
         Board newBoard = new Board();
         newBoard.boardTable[0][2] = "O";
         newBoard.boardTable[1][0] = "X";
