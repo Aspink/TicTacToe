0b8bf92c9b25c15556e6dfa7edd6be9498b6441c
Aspink <aspink1@wp.pl>
2023-02-08 21:58:15 +0100

    ClearScreen test

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         Board newBoard = new Board();
+        ClearScreen.cls();
         newBoard.boardTable[0][2] = "O";
         newBoard.boardTable[1][0] = "X";
         newBoard.boardPrinting();
         newBoard.boardTable[1][1] = "O";
         newBoard.boardTable[2][0] = "X";
+        ClearScreen.cls();
         newBoard.boardPrinting();
         newBoard.boardTable[0][0] = "O";
         newBoard.boardTable[2][2] = "X";
+        ClearScreen.cls();
         newBoard.boardPrinting();
 
 
