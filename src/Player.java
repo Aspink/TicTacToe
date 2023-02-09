6dd05ec810870e4ebbf1f1abe3e2f9496ba9bf0c
Aspink <aspink1@wp.pl>
2023-02-09 21:06:51 +0100

    Player class properties added

--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,16 @@
 public class Player {
+    String name;
+    String sign;
+
+    public Player() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSign() {
+        return sign;
+    }
 }
