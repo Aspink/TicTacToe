508aec70b27caab2bbbe3ecd7d67bf281ee23a03
Aspink <aspink1@wp.pl>
2023-02-15 21:49:20 +0100

    some correction in InOutPut

--- src/InOutPut.java
+++ src/InOutPut.java
@@ -2,7 +2,14 @@ import java.util.Scanner;
 
 public class InOutPut {
 
-    public InOutPut() {
+    public void InOutPut() {
         Scanner input = new Scanner(System.in);
+    }
+
+    public String choiceSign() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Wybierasz kółko (O), krzyżyk (X) czy losowanie (L)?");
+        String sign = input.nextLine();
+        return sign;
     }
 }
