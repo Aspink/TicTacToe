6965ec8a7ac1caa5544da39496b800dae3d4e65c
Aspink <aspink1@wp.pl>
2023-02-22 21:35:28 +0100

    Random sign and confirmation in OnOutPut

--- src/InOutPut.java
+++ src/InOutPut.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-
+import java.util.Random;
 public class InOutPut {
 
     public void InOutPut() {
@@ -8,11 +8,20 @@ public class InOutPut {
 
     public String choiceSign() {
         Scanner input = new Scanner(System.in);
+        Random random = new Random();
         String sign = " ";
         while(!sign.equals("O") && !sign.equals("X") && !sign.equals("L")) {
             System.out.println("Wybierasz kółko (O), krzyżyk (X) czy losowanie (L)?");
             sign = input.nextLine().toUpperCase();
         }
+        if(sign.equals("L")) {
+            sign = "X";
+            int rnd = random.nextInt(2);
+            if(rnd == 0) {
+                sign = "O";
+            }
+        }
+        System.out.println("Twój znak to " + sign);
         return sign;
     }
 }
