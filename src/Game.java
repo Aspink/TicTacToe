5389b072d6df83982c8d25baea51ff809a071351
Aspink <aspink1@wp.pl>
2023-02-24 12:52:36 +0100

    Responsibilities moved to Game class

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,12 @@
 public class Game {
+    Board newBoard = new Board();
+    public Player player1 = new Player();
+    InOutPut userInterface = new InOutPut();
+    public void Game() {}
+    public void start() {
+        player1.setName(userInterface.whatsYourName());
+        player1.setSign(userInterface.choiceSign());
+    }
+
 
 }
