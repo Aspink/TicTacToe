6063bf61b0555ab78106605d99c0b0815eb67ee4
Aspink <aspink1@wp.pl>
2023-02-24 12:54:54 +0100

    Responsibilities removed from TicTacToe class, game object created.

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,14 +1,7 @@
-import java.util.Scanner;
-
 public class TicTacToe {
-    Board newBoard = new Board();
-    public static Player player1 = new Player();
-    Player player2 = new Player();
-
     public static void main(String[] args) {
-        InOutPut userInterface = new InOutPut();
-        player1.setSign(userInterface.choiceSign());
-        player1.setName(userInterface.whatsYourName());
+        Game game = new Game();
+        game.start();
     }
 }
 
