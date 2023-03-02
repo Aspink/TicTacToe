f42ef1ed778d93eb6b5d2c78128a88b407075939
Aspink <aspink1@wp.pl>
2023-03-02 16:11:49 +0100

    Some code cleaning in InOutPut class

--- src/InOutPut.java
+++ src/InOutPut.java
@@ -2,31 +2,30 @@ import java.util.Scanner;
 import java.util.Random;
 public class InOutPut {
     public static final String RESET = "\033[0m";
-    public static final String BLACK = "\033[1;30";
     public static final String RED = "\033[1;31m";
     public static final String GREEN = "\033[1;32m";
     public static final String YELLOW = "\033[1;33m";
-    public static final String BLUE = "\033[1;34m";
-    public static final String PURPLE = "\033[1;35m";
     public static final String CYAN = "\033[1;36m";
-    public static final String WHITE = "\033[1;37m";
-    public void InOutPut() {
-
-    }
 
     public String choiceSign() {
         Scanner input = new Scanner(System.in);
         Random random = new Random();
         String sign = " ";
-        while(!sign.equals("O") && !sign.equals("X") && !sign.equals("L")) {
+        while(!sign.equals("〇") && !sign.equals("⛌") && !sign.equals("L")) {
             System.out.println(YELLOW + "Wybierasz kółko (O), krzyżyk (X) czy losowanie (L)?" + RESET);
             sign = input.nextLine().toUpperCase();
+            if(sign.equals("O")) {
+                sign = "〇";
+            }
+            if(sign.equals("X")) {
+                sign = "⛌";
+            }
         }
         if(sign.equals("L")) {
-            sign = "X";
+            sign = "⛌";
             int rnd = random.nextInt(2);
             if(rnd == 0) {
-                sign = "O";
+                sign = "〇";
             }
         }
         System.out.println(YELLOW + "Twój znak to " + sign + "."+ RESET);
@@ -34,7 +33,6 @@ public class InOutPut {
     }
     public static String whatsYourName(String color) {
         Scanner input = new Scanner(System.in);
-        Random random = new Random();
         String name;
         System.out.println(color + "Podaj swoje imię!" + RESET);
         name = input.nextLine();
@@ -43,7 +41,6 @@ public class InOutPut {
     }
     public String choiceOpponent() {
         Scanner input = new Scanner(System.in);
-        Random random = new Random();
         String enemy = " ";
         while(!enemy.equals("K") && !enemy.equals("C")) {
             System.out.println(YELLOW + "Grasz ze mną (K) czy z człowiekiem (C)?" + RESET);
@@ -53,18 +50,16 @@ public class InOutPut {
     }
     public static int choiceColumn() {
         Scanner input = new Scanner(System.in);
-        int column = 0;
+        int column;
         System.out.print("Wybierz kolumnę (od 1 do 3): ");
         column = input.nextInt();
-//        System.out.println(color + "Witaj, " + name + "!" + RESET);
         return column;
     }
     public static int choiceRow() {
         Scanner input = new Scanner(System.in);
-        int row = 0;
+        int row;
         System.out.print("Wybierz wiersz (od 1 do 3): ");
         row = input.nextInt();
-//        System.out.println(color + "Witaj, " + name + "!" + RESET);
         return row;
     }
 }
